78fc6137b34915c19394309a9b63200b7df44cb6
Leta Keane <letakeane+turing@gmail.com>
2019-06-30 13:34:19 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.locals.title = 'IdeaBox API';
+app.locals.ideas = [
+  {id: 1, title: 'Sweaters for pugs', description: 'To keep them warm'},
+  {id: 2, title: 'Film a romcom', description: 'But make it ghosts'},
+  {id: 3, title: 'A game show called Ether/Or', description: 'When you lose you get chloroformed'},
+];
+
+app.set('port', 3001);
+
+app.get('/api/v1/ideas', (request, response) => {
+  response.status(200).json(app.locals.ideas);
+});
+
+app.get('/api/v1/ideas/:id', (request, response) => {
+  const { id } = request.params;
+  const match = app.locals.ideas.find(idea => idea.id == id);
+
+  if (!match) return response.status(404).json({error: `No idea found with an id of ${id}`});
+
+  return response.status(200).json(match);
+});
+
+app.post('/api/v1/ideas', (request, response) => {
+  const newIdea = request.body;
+
+  for (let requiredParameter of ['id', 'title', 'description']) {
+    if (!newIdea[requiredParameter]) return response.status(422).json({error: `You are missing a required parameter of ${requiredParameter}`});
+  }
+
+  app.locals.ideas = [...app.locals.ideas, newIdea];
+
+  return response.status(201).json({id: newIdea.id});
+});
+
+app.delete('/api/v1/ideas/:id', (request, response) => {
+  const { id } = request.params;
+  const match = app.locals.ideas.find(idea => idea.id == id);
+
+  if (!match) return response.status(404).json({error: `No idea found with an id of ${id}`});
+
+  const filteredIdeas = app.locals.ideas.filter(idea => idea.id != id);
+
+  app.locals.ideas = filteredIdeas;
+
+  return response.sendStatus(204);
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is now running on port ${app.get('port')}!`);
+});
